c57778b4c2338ba0f43a5a7fd1e59f097647e59d
Sungin Jung <jungs@uchicago.edu>
2017-08-22 15:34:51 -0500

    finish layout spacing

--- apps/pinterest/static/pinterest/js/pin.js
+++ apps/pinterest/static/pinterest/js/pin.js
@@ -4,6 +4,6 @@ $(document).ready(function () {
         columnWidth: '.grid-sizer',
         percentPosition: true,
         horizontalOrder: true,
-        gutter: 5%
+        gutter: '.gutter-sizer'
     })
 });
